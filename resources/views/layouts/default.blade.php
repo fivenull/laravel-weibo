9d4e29427421ae439198a22545eb1761c025208b
fivenull <chenzhiyuan@fivenulldeMacBook-Pro.local>
2019-10-25 09:57:09 +0800

    test master

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,13 @@
 <html>
 <head>
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-@yield('content')
+   @include("layouts._header")
+    <div class="container">
+        @yield('content')
+        @include("layouts._footer")
+    </div>
 </body>
 </html>
